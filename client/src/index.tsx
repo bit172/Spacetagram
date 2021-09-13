ff3551496d72ec467b369b6869f25e3e913e37dd
Jonathan <bit172@gmail.com>
2021-09-13 13:43:26 -0400

    feat: remove css for css-in-js

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "normalize.css";
+import "@fontsource/open-sans";
+import "@fontsource/open-sans/600.css";
+
 import App from "./App";
+
+import GlobalStyles from "./styles/global.styles";
+import Typography from "./styles/typography.styles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
+    <Typography />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
