d48539556a1eb61a3244d97647f65d0f788a6810
Jonathan <bit172@gmail.com>
2021-09-13 13:43:56 -0400

    chore: add temp sample data for client

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import db from "./config/database";
-import nasaApiInstance from "./nasa-api";
+// import nasaApiInstance from "./nasa-api";
 import path from "path";
+import { sampleData } from "./sample-data";
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
@@ -18,11 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/api/", async (req, res) => {
-  const nasaRes = await nasaApiInstance({
-    method: "GET",
-    params: { start_date: "2021-09-01", end_date: "2021-09-10" },
-  });
-  res.json(nasaRes.data);
+  // const nasaRes = await nasaApiInstance({
+  //   method: "GET",
+  //   params: { start_date: "2021-09-01", end_date: "2021-09-10" },
+  // });
+  res.json(sampleData);
 });
 
 if (process.env.NODE_ENV === "production") {
