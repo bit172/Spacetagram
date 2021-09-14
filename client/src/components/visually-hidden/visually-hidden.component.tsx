dba79ea4679a42c17ddf8ee35a94ce8f0e821fc4
Jonathan Hsu <bit172@gmail.com>
2021-09-14 16:15:55 -0400

    feat: add visually hidden component

---
+++ client/src/components/visually-hidden/visually-hidden.component.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+// Source: https://www.joshwcomeau.com/snippets/react-components/visually-hidden/
+
+const VisuallyHiddenSpan = styled.span`
+  display: inline-block;
+  position: absolute;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  height: 1;
+  width: 1;
+  margin: -1;
+  padding: 0;
+  border: 0;
+`;
+
+export interface VisuallyHiddenProps {
+  children: any;
+}
+
+export default function VisuallyHidden({
+  children,
+  ...delegated
+}: VisuallyHiddenProps) {
+  const [forceShow, setForceShow] = useState(false);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'production') {
+      const handleKeyDown = (ev: KeyboardEvent) => {
+        if (ev.key === 'Alt') {
+          setForceShow(true);
+        }
+      };
+      const handleKeyUp = (ev: KeyboardEvent) => {
+        if (ev.key === 'Alt') {
+          setForceShow(false);
+        }
+      };
+      window.addEventListener('keydown', handleKeyDown);
+      window.addEventListener('keyup', handleKeyUp);
+      return () => {
+        window.removeEventListener('keydown', handleKeyDown);
+        window.removeEventListener('keydown', handleKeyUp);
+      };
+    }
+  }, []);
+
+  if (forceShow) {
+    return children;
+  }
+
+  return <VisuallyHiddenSpan {...delegated}>{children}</VisuallyHiddenSpan>;
+}
