c8179304223a47116087cd5df359cbc8378c1dd9
Jonathan Hsu <bit172@gmail.com>
2021-09-15 00:23:59 -0400

    feat: implement spinner and skip nav to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import Post from './components/post/post.component';
+import { SkipNavContent, SkipNavLink } from '@reach/skip-nav';
+
 import { APOD } from './interfaces/apod.interface';
+
+import Post from './components/post/post.component';
+import Spinner from './components/spinner/spinner.component';
+
+import '@reach/skip-nav/styles.css';
 
 function App() {
   const [apods, setAPODs] = useState<APOD[]>([]);
@@ -11,15 +17,24 @@ function App() {
       .then(data => setAPODs(data));
   }, []);
 
-  if (!apods) return <div>Loading...</div>;
-
   return (
     <React.Fragment>
       <main className="container">
+        <SkipNavLink />
+        <h1>
+          Spacetagram
+          <span role="img" aria-label="rocket">
+            🚀
+          </span>
+        </h1>
+        <p>Brought to you by NASA's Astronomy Photo of the Day (APOD) API</p>
+        <SkipNavContent />
         <section>
-          {apods.map(apod => (
-            <Post apod={apod} />
-          ))}
+          {apods.length ? (
+            apods.map(apod => <Post key={apod.date} apod={apod} />)
+          ) : (
+            <Spinner size={50} />
+          )}
         </section>
       </main>
     </React.Fragment>
